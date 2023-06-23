c6cb949e6527bb179cb753d08fd11da9a3a1cf05
Дрійко КваКва <kupchak.a.m@gmail.com>
2023-06-23 21:48:06 +0300

    remove data seeds

--- src/seeds/datas/artists.data.ts
+++
@@ -1,6 +0,0 @@
-export const artists = [
-  {
-    name: 'Artist Tets',
-    nationality: 'Nationality test',
-  },
-];
--- src/seeds/datas/users.data.ts
+++
@@ -1,6 +0,0 @@
-export const users = [
-  {
-    name: 'User',
-    telegram_id: '111111111',
-  },
-];
