b0893d91a7ecae20ff4ae981f0508d22d0066154
Дрійко КваКва <kupchak.a.m@gmail.com>
2023-08-08 18:36:41 +0300

    added REST API, updated docker-compose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ArtistService } from './bot/services/artist.service';
+import { Artist } from './bot/entities/artist.entity';
 
-@Controller()
+@Controller('artist')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly artistService: ArtistService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':name') findByName(@Param('name') name: string): Promise<Artist> {
+    const artistName = name.toLocaleLowerCase();
+    return this.artistService.findOne(artistName);
   }
 }
