b9286cf76f3ef14c1bbf49c51d9b53f6747ce79d
Дрійко КваКва <kupchak.a.m@gmail.com>
2024-10-24 09:58:38 +0300

    resolved package conflict

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
   app.enableCors();
 
-  await app.listen(80 || 3100);
+  await app.listen(3100);
 }
 bootstrap();
